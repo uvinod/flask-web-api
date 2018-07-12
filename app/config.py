5ed346d4a4785a6ac0770901e6ac278df7f4704e
Vinod Kumar <vinod.kumar@ip-tribe.com>
2018-07-12 17:30:33 +0800

    change contact response

--- app/config.py
+++ app/config.py
@@ -1,10 +1,10 @@
 import os
 
 base_dir = os.path.abspath(os.path.dirname(__file__))
-#local_base = 'postgresql://postgres:Welcome30@localhost:5432/'
-local_base = 'postgres://appyisypegdoef:92fca4b6df0ffe561d39164a57b90ec4a7e6267d7f066c5344242dbb2982f99d@ec2-54-227-240-7.compute-1.amazonaws.com:5432/'
-#database_name = 'chatbotservice'
-database_name = 'd3kcjb36e6b19a'
+local_base = 'postgresql://postgres:Welcome30@localhost:5432/'
+#local_base = 'postgres://appyisypegdoef:92fca4b6df0ffe561d39164a57b90ec4a7e6267d7f066c5344242dbb2982f99d@ec2-54-227-240-7.compute-1.amazonaws.com:5432/'
+database_name = 'chatbotservice'
+#database_name = 'd3kcjb36e6b19a'
 
 
 class BaseConfig:
